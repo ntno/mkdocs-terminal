ea3e25bb5424d262ea01e1db9cd213ff2bff450c
ntno <ntno@users.noreply.github.com>
2023-01-11 13:39:22 -0500

    future test for breaking change

--- tests/test_tile.py
+++ tests/test_tile.py
@@ -1,6 +1,7 @@
 from tests.utils.tile import Tile
 from tests.utils.html import check_html
-
+from tests import defaults
+import pytest
 
 class TestTile():
 
@@ -21,6 +22,7 @@ class TestTile():
         assert "<a " in rendered_tile
         assert "</a>" in rendered_tile
         assert "<img " not in rendered_tile
+        assert "id=" not in rendered_tile
         # TODO assert contains <div/figure/a
         assert len(check_html(rendered_tile)["errors"]) == 0
 
@@ -32,6 +34,7 @@ class TestTile():
         assert "<a " not in rendered_tile
         assert "</a>" not in rendered_tile
         assert "<img " in rendered_tile
+        assert "id=" not in rendered_tile
         # TODO assert contains <div/figure/img>
         assert len(check_html(rendered_tile)["errors"]) == 0
 
@@ -43,5 +46,16 @@ class TestTile():
         assert "<a " in rendered_tile
         assert "</a>" in rendered_tile
         assert "<img " in rendered_tile
+        assert "id=" not in rendered_tile
         # TODO assert contains <div/figure/a/img>
+        assert len(check_html(rendered_tile)["errors"]) == 0
+
+    @pytest.mark.skip(reason="breaking change needs to be fixed in major release")
+    def test_id_and_class_added_to_tile(self, env_with_terminal_loader):
+        tile = Tile(html_id="myId", css_class="myClass", link_href=defaults.GITHUB_LINK_HREF)
+        tile_macro = env_with_terminal_loader.get_template("macros/tile.j2")
+        rendered_tile = tile_macro.module.make_tile(tile)
+        assert rendered_tile.strip() != ""
+        assert "id=\"myID\"" in rendered_tile
+        assert "class=\"terminal-mkdocs-tile myClass\"" in rendered_tile
         assert len(check_html(rendered_tile)["errors"]) == 0
