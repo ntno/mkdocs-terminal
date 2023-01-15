eaf7887e4cbddae700e24edda24a385f8929a02c
ntno <ntno@users.noreply.github.com>
2023-01-15 16:45:53 -0500

    wip

---
+++ tests/test_search.py
@@ -0,0 +1,44 @@
+from tests.utils import theme_plugins
+from jinja2 import Template, pass_context
+import pytest
+
+
+@pytest.fixture
+def enabled_context():
+    nav_list = []
+    return {
+        "nav": nav_list,
+        "config": {
+            "site_name": "site_name_placeholder",
+            "site_url": "site_url_placeholder",
+            "plugins": [theme_plugins.SEARCH]
+        },
+        "button_idx": len(nav_list)
+    }
+
+
+@pytest.fixture
+def search_button_partial(env_with_terminal_loader):
+    return env_with_terminal_loader.get_template("partials/top/search-button.html")
+
+
+class TestSearchButton():
+    @pytest.mark.parametrize("plugins_list", [
+        pytest.param(
+            None, id="null_plugins_list"
+        ),
+        pytest.param(
+            [], id="empty_plugins_list"
+        )
+    ])
+    def test_no_search_button_when_search_plugin_disabled(self, search_button_partial, enabled_context, plugins_list):
+        enabled_context["config"]["plugins"] = plugins_list
+        context_data = enabled_context
+        rendered_nav = search_button_partial.render(context_data)
+        assert "#mkdocs_search_modal" not in rendered_nav
+
+
+    def test_search_button_included_when_search_plugin_enabled(self, search_button_partial, enabled_context):
+        context_data = enabled_context
+        rendered_nav = search_button_partial.render(context_data)
+        assert "#mkdocs_search_modal" in rendered_nav
