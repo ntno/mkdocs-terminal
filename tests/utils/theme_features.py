c142de23f40741ab2c2b97c973647208aea61248
ntno <ntno@users.noreply.github.com>
2023-01-15 14:12:53 -0500

    formatting

--- tests/utils/theme_features.py
+++ tests/utils/theme_features.py
@@ -2,6 +2,6 @@
 SHOW_REVISION_DATE = "revision.date"
 SHOW_REVISION_HISTORY = "revision.history"
 
-# page features
+# page specific features
 HIDE_REVISION_DATE_ON_PAGE = "revision_date"
 HIDE_REVISION_HISTORY_ON_PAGE = "revision_history"
