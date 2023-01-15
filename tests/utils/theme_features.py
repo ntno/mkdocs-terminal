4f34b7788ad535f70005e5684c3f2aa591732196
ntno <ntno@users.noreply.github.com>
2023-01-15 13:54:05 -0500

    test setup for page specific revision hiding

--- tests/utils/theme_features.py
+++ tests/utils/theme_features.py
@@ -3,5 +3,5 @@ SHOW_REVISION_DATE = "revision.date"
 SHOW_REVISION_HISTORY = "revision.history"
 
 # page features
-HIDE_REVISION_DATE_ON_PAGE = ""
-HIDE_REVISION_HISTORY_ON_PAGE = ""
+HIDE_REVISION_DATE_ON_PAGE = "revision_date"
+HIDE_REVISION_HISTORY_ON_PAGE = "revision_history"
