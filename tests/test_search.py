6fb536f08f3f2732b0bf84c5cde3b63a9a13f2b8
ntno <ntno@users.noreply.github.com>
2023-01-15 16:56:06 -0500

    wip

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,4 @@
-from tests.utils import theme_plugins
-from jinja2 import Template, pass_context
+from tests.utils import theme_plugins, theme_features
 import pytest
 
 
@@ -11,7 +10,10 @@ def enabled_context():
         "config": {
             "site_name": "site_name_placeholder",
             "site_url": "site_url_placeholder",
-            "plugins": [theme_plugins.SEARCH]
+            "plugins": [theme_plugins.SEARCH],
+            "theme": {
+                "features": []
+            }
         },
         "button_idx": len(nav_list)
     }
@@ -35,10 +37,16 @@ class TestSearchButton():
         enabled_context["config"]["plugins"] = plugins_list
         context_data = enabled_context
         rendered_nav = search_button_partial.render(context_data)
-        assert "#mkdocs_search_modal" not in rendered_nav
+        assert rendered_nav == ""
 
 
     def test_search_button_included_when_search_plugin_enabled(self, search_button_partial, enabled_context):
         context_data = enabled_context
         rendered_nav = search_button_partial.render(context_data)
         assert "#mkdocs_search_modal" in rendered_nav
+
+    def test_search_button_not_included_when_feature_disabled(self, search_button_partial, enabled_context):
+        enabled_context["config"]["theme"]["features"] = [theme_features.HIDE_SEARCH_BUTTON]
+        context_data = enabled_context
+        rendered_nav = search_button_partial.render(context_data)
+        assert rendered_nav == ""
