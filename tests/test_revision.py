1293ffb06d9bc0261936e0c456d51b58014f8e4f
ntno <ntno@users.noreply.github.com>
2023-01-15 12:32:32 -0500

    test refactor

--- tests/test_revision.py
+++ tests/test_revision.py
@@ -3,6 +3,25 @@ from tests.utils import theme_features, theme_plugins
 import pytest
 
 
+@pytest.fixture
+def revsion_fully_enabled_context():
+    context = {
+        "page": {
+            "meta": {
+                "edit_url": "edit_url_placeholder",
+                "revision_date": "revision_date_placeholder"
+            }
+        },
+        "config": {
+            "repo_name": "repo_name_placeholder",
+            "repo_url": "repo_url_placeholder",
+            "plugins": [theme_plugins.REVISION]
+        },
+        "features": [theme_features.SHOW_REVISION_DATE, theme_features.SHOW_REVISION_HISTORY]
+    }
+    return context
+
+
 class TestRevision():
     @pytest.mark.parametrize("plugins_list", [
         pytest.param(
@@ -43,6 +62,22 @@ class TestRevision():
         except Exception as ex:
             pytest.fail(f"Got exception during render: {ex})")
 
+    def test_no_content_when_theme_features_disabled(self, env_with_terminal_loader):
+        revision_partial = env_with_terminal_loader.get_template("partials/revision.html")
+        context_data = {
+            "page": {
+                "meta": {
+                    "revision_date": "2023/04/01"
+                }
+            },
+            "config": {
+                "plugins": [theme_plugins.REVISION]
+            },
+            "features": []
+        }
+        rendered_revision = revision_partial.render(context_data)
+        assert rendered_revision.strip() == ""
+
     def test_that_revision_renders_when_no_page_meta(self, env_with_terminal_loader):
         revision_partial = env_with_terminal_loader.get_template("partials/revision.html")
         context_data = {
@@ -65,62 +100,38 @@ class TestRevision():
         rendered_revision = revision_partial.render(context_data)
         assert rendered_revision.strip() == ""
 
-    def test_that_last_updated_text_when_page_has_revision_date(self, env_with_terminal_loader):
+    def test_that_last_updated_text_when_page_has_revision_date(self, env_with_terminal_loader, revsion_fully_enabled_context):
         revision_partial = env_with_terminal_loader.get_template("partials/revision.html")
-        context_data = {
-            "page": {
-                "meta": {
-                    "revision_date": "2023/01/01"
-                }
-            },
-            "config": {
-                "plugins": [theme_plugins.REVISION]
-            }
-        }
+        revsion_fully_enabled_context["page"]["meta"]["revision_date"] = "2023/01/01"
+        context_data = revsion_fully_enabled_context
         rendered_revision = revision_partial.render(context_data)
         assert "Page last updated 2023/01/01" in rendered_revision
         assert_valid_html(rendered_revision)
 
-    def test_that_github_history_link_added_when_site_has_page_edit_url_and_repo_name_set(self, env_with_terminal_loader):
+    def test_that_github_history_link_added_when_site_has_page_edit_url_and_repo_name_set(self, env_with_terminal_loader, revsion_fully_enabled_context):
         revision_partial = env_with_terminal_loader.get_template("partials/revision.html")
         mkdocs_generated_page_url = "https://github.com/myUsername/myRepository/edit/main/docs/index.md"
         expected_page_history_url = "https://github.com/myUsername/myRepository/commits/main/docs/index.md"
         mkdocs_generated_repo_name = "GitHub"
-        context_data = {
-            "page": {
-                "edit_url": mkdocs_generated_page_url,
-                "meta": {
-                    "revision_date": "2023/01/02"
-                }
-            },
-            "config": {
-                "plugins": [theme_plugins.REVISION],
-                "repo_name": mkdocs_generated_repo_name
-            }
-        }
+        revsion_fully_enabled_context["page"]["meta"]["revision_date"] = "2023/01/02"
+        revsion_fully_enabled_context["page"]["meta"]["edit_url"] = mkdocs_generated_page_url
+        revsion_fully_enabled_context["config"]["repo_name"] = mkdocs_generated_repo_name
+        context_data = revsion_fully_enabled_context
         rendered_revision = revision_partial.render(context_data)
         assert "Page last updated 2023/01/02" in rendered_revision
         assert "See revision history on" in rendered_revision
         assert "<a target=\"_blank\" href=\"" + expected_page_history_url + "\">GitHub</a>" in rendered_revision
         assert_valid_html(rendered_revision)
 
-    def test_that_bitbucket_source_link_added_when_site_has_page_edit_url_and_repo_name_set(self, env_with_terminal_loader):
+    def test_that_bitbucket_source_link_added_when_site_has_page_edit_url_and_repo_name_set(self, env_with_terminal_loader, revsion_fully_enabled_context):
         revision_partial = env_with_terminal_loader.get_template("partials/revision.html")
         mkdocs_generated_page_url = "https://bitbucket.org/myUsername/myRepository/src/main/docs/index.md?mode=edit"
         expected_page_source_url = "https://bitbucket.org/myUsername/myRepository/src/main/docs/index.md?mode=read"
         mkdocs_generated_repo_name = "Bitbucket"
-        context_data = {
-            "page": {
-                "edit_url": mkdocs_generated_page_url,
-                "meta": {
-                    "revision_date": "2023/03/04"
-                }
-            },
-            "config": {
-                "plugins": [theme_plugins.REVISION],
-                "repo_name": mkdocs_generated_repo_name
-            }
-        }
+        revsion_fully_enabled_context["page"]["meta"]["revision_date"] = "2023/03/04"
+        revsion_fully_enabled_context["page"]["meta"]["edit_url"] = mkdocs_generated_page_url
+        revsion_fully_enabled_context["config"]["repo_name"] = mkdocs_generated_repo_name
+        context_data = revsion_fully_enabled_context
         rendered_revision = revision_partial.render(context_data)
         assert "Page last updated 2023/03/04" in rendered_revision
         assert "See revision history on" in rendered_revision
