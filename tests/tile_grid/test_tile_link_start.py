501bd2cbeef90ec2aaca8d5338eeed11b9695e37
ntno <ntno@users.noreply.github.com>
2023-01-16 17:37:36 -0500

    test refactor

--- tests/tile_grid/test_tile_link_start.py
+++ tests/tile_grid/test_tile_link_start.py
@@ -2,16 +2,19 @@ from tests import defaults
 import pytest
 
 
+@pytest.fixture
+def tile_link_macro(env_with_terminal_loader):
+    return env_with_terminal_loader.get_template("macros/tile-grid/tile-link.j2")
+
+
 class TestTileLinkHelper():
 
-    def test_that_link_start_contains_href_with_minimal_link_only_tile(self, env_with_terminal_loader, minimal_link_tile):
-        tile_link_macro = env_with_terminal_loader.get_template("macros/tile-grid/tile-link.j2")
+    def test_that_link_start_contains_href_with_minimal_link_only_tile(self, tile_link_macro, minimal_link_tile):
         rendered_link_start = tile_link_macro.module.make_link_start(minimal_link_tile)
         assert defaults.GITHUB_LINK_HREF in rendered_link_start
 
-    def test_that_link_start_fragment_renders_with_integer_inputs(self, env_with_terminal_loader, all_integer_tile):
-        link_macro = env_with_terminal_loader.get_template("macros/tile-grid/tile-link.j2")
+    def test_that_link_start_fragment_renders_with_integer_inputs(self, tile_link_macro, all_integer_tile):
         try:
-            link_macro.module.make_link_start(all_integer_tile)
+            tile_link_macro.module.make_link_start(all_integer_tile)
         except Exception as ex:
             pytest.fail(f"Got exception during render: {ex})")
