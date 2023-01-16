2095f1d91fe4b08cdb5fefccfbe450d355b1c0c2
ntno <ntno@users.noreply.github.com>
2023-01-16 14:06:46 -0500

    toc refactor wip

--- tests/interface/page_features.py
+++ tests/interface/page_features.py
@@ -2,3 +2,4 @@
 HIDE_REVISION_DATE_ON_PAGE = "revision_date"
 HIDE_REVISION_HISTORY_ON_PAGE = "revision_history"
 HIDE_TOP_NAV_ON_PAGE = "top_nav"
+HIDE_SIDE_TOC_ON_PAGE = "side_toc"
---
+++ tests/side_panel/test_side_toc.py
@@ -0,0 +1,59 @@
+from tests.interface import theme_features, page_features
+from tests.utils.html import assert_valid_html
+import pytest
+
+
+@pytest.fixture
+def enabled_context():
+    toc_list = [
+         {
+            "title": "first_header_placeholder",
+            "level": 0,
+            "url": "anchor_to_first_header_placeholder"
+        }
+    ]
+    return {
+        "page": {
+            "page": {
+                "toc": toc_list,
+                "content": "page_conent_placeholder"
+            },
+            "meta": {
+                "hide": []
+            }
+        },
+        "config": {
+            "theme": {
+                "features": []
+            }
+        }
+    }
+
+
+@pytest.fixture
+def side_toc_partial(env_with_terminal_loader):
+    return env_with_terminal_loader.get_template("partials/side-panel/side-toc.html")
+
+
+class TestSideToc():
+    def test_no_content_when_theme_feature_enabled(self, side_toc_partial, enabled_context):
+        enabled_context["config"]["theme"]["features"] = [theme_features.HIDE_SIDE_TOC]
+        context_data = enabled_context
+        rendered_side_toc = side_toc_partial.render(context_data)
+        assert rendered_side_toc == ""
+
+    def test_no_content_when_page_feature_enabled(self, side_toc_partial, enabled_context):
+        enabled_context["page"]["meta"]["hide"] = [page_features.HIDE_SIDE_TOC_ON_PAGE]
+        context_data = enabled_context
+        rendered_side_toc = side_toc_partial.render(context_data)
+        assert rendered_side_toc == ""
+
+    def test_no_entries_when_page_has_no_headers(self, side_toc_partial, enabled_context):
+        pass
+
+    def test_has_entries_when_page_has_headers(self, side_toc_partial, enabled_context):
+        context_data = enabled_context
+        rendered_side_toc = side_toc_partial.render(context_data)
+        assert "<hr>" in rendered_side_toc
+        assert "first_title_placeholder" in rendered_side_toc
+        assert_valid_html(rendered_side_toc)
