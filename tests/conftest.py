2678a3bc7948852c6b575f8abf8b13c7ec48dd4b
ntno <ntno@users.noreply.github.com>
2023-01-18 17:13:49 -0500

    wip - fix for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from jinja2.environment import Environment
 from tests.interface.tile import Tile
 from tests import defaults
 from tests.utils.filters import mock_url_filter
+from terminal.plugins.md_to_html.plugin import md_filter
 import pytest
 
 
@@ -12,6 +13,7 @@ def env():
     """returns a new environment with mock mkdocs url filter."""
     env = Environment()
     env.filters['url'] = mock_url_filter
+    env.filters['markdown'] = md_filter
     return env
 
 
