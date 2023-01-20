e4515d851a974a12f209136f0e4c2a42b7dc539f
ntno <ntno@users.noreply.github.com>
2023-01-20 12:43:29 -0500

    default to empty string

--- terminal/pluglets/tile_grid/tile.py
+++ terminal/pluglets/tile_grid/tile.py
@@ -6,7 +6,7 @@ ATTRIBUTES_TO_MARKUP= ["caption", "link_text"]
 class Tile:
     
     def __init__(
-        self, caption: Optional[Any], img_src: Optional[Any], img_title: Optional[Any], img_alt: Optional[Any], img_width: Optional[Any], img_height: Optional[Any], link_href: Optional[Any], link_target: Optional[Any], link_text: Optional[Any], link_title: Optional[Any], tile_id: Optional[Any], tile_css: Optional[Any]
+        self, caption: Optional[Any]="", img_src: Optional[Any]="", img_title: Optional[Any]="", img_alt: Optional[Any]="", img_width: Optional[Any]="", img_height: Optional[Any]="", link_href: Optional[Any]="", link_target: Optional[Any]="", link_text: Optional[Any]="", link_title: Optional[Any]="", tile_id: Optional[Any]="", tile_css: Optional[Any]=""
     ) -> None:
         self.caption = caption
         self.img_src = img_src
@@ -30,49 +30,42 @@ class Tile:
             self.img_src is not None) else '[blank]'
         return f"Tile(caption={caption}, link_href={link_href}, img_src='{img_src}')"
 
-    # # TODO - 
-    # def markup(self):
-    #     for attribute in ATTRIBUTES_TO_MARKUP:
-    #         if self[attribute] is not None:
-    #             self[attribute] = "MARKED-" + self[attribute]
+    def validate(self):
+        print("this is a test", self)
+        pass
 
-
-    # def render(self):
-    #     pass 
-
-
-    caption: Optional[Any]
+    caption: Optional[Any]=""
     """The figure caption."""
 
-    img_src: Optional[Any]
+    img_src: Optional[Any]=""
     """The image source.  Can be an external image like `https://picsum.photos/id/167/200/200` or an internal MkDocs image like `../img/palettes/default.png`."""
 
-    img_title: Optional[Any]
+    img_title: Optional[Any]=""
     """Text to display on hover."""
 
-    img_alt: Optional[Any]
+    img_alt: Optional[Any]=""
     """Alternate text for the image if the image cannot be displayed."""
 
-    img_width: Optional[Any]
+    img_width: Optional[Any]=""
     """Width to set on the image element."""
 
-    img_height: Optional[Any]
+    img_height: Optional[Any]=""
     """Height to set on the image element."""
 
-    link_href: Optional[Any]
+    link_href: Optional[Any]=""
     """Web page URL.  Can be an external web page or an internal MkDocs page like `./default/`."""
 
-    link_target: Optional[Any]
+    link_target: Optional[Any]=""
     """Specifies where to open the linked webpage.  `_blank` will open the link in a new tab.  `_self` will open the link in the current window."""
 
-    link_text: Optional[Any]
+    link_text: Optional[Any]=""
     """Text to display for a [link only] tile.  Ignored if `img_src` is specified."""
 
-    link_title: Optional[Any]
+    link_title: Optional[Any]=""
     """Text to display on hover.  Should not be used if `img_title` is already specified."""
 
-    tile_id: Optional[Any]
+    tile_id: Optional[Any]=""
     """ID to add to the tile's HTML for advanced styling."""
 
-    tile_css: Optional[Any]
+    tile_css: Optional[Any]=""
     """CSS class to add to the tile's HTML for advanced styling."""
