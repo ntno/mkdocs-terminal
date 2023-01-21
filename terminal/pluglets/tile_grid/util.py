da6630684cb046ce1db97bced0bed97d43674606
ntno <ntno@users.noreply.github.com>
2023-01-20 22:09:37 -0500

    initial mocking working

--- terminal/pluglets/tile_grid/util.py
+++ terminal/pluglets/tile_grid/util.py
@@ -3,14 +3,8 @@ DEFAULT_GRID_PARTIAL_PATH = "pluglets/tile_grid/templates/j2-partials/tiles.html
 USAGE_MESSAGE = "<strong>USAGE:</strong> `{{ tile_grid(page.meta) }}`"
 
 
-def tile_grid(page_meta={}):
-    if page_meta is None:
-        return USAGE_MESSAGE
-    if page_meta is not dict:
-        return USAGE_MESSAGE
-    if page_meta is {}:
-        return USAGE_MESSAGE
-    else:
+def tile_grid(page_meta):
+    if page_meta is not None and isinstance(page_meta, dict) and len(page_meta.keys()) > 0:
         context_data = {
             "page": {
                 "meta": copy(page_meta)
@@ -20,3 +14,4 @@ def tile_grid(page_meta={}):
         tiles_partial = MACRO.jinja2_env.get_template(DEFAULT_GRID_PARTIAL_PATH)
         rendered_grid = tiles_partial.render(context_data)
         return rendered_grid
+    return USAGE_MESSAGE
--- tests/pluglets/test_tile_grid_pluglet.py
+++ tests/pluglets/test_tile_grid_pluglet.py
@@ -1,13 +1,17 @@
 from terminal.pluglets.tile_grid.util import tile_grid
+from tests.utils.html import assert_valid_html
+from unittest.mock import patch, MagicMock
 import pytest
+
+
+@pytest.fixture
+def grid_partial(env_with_terminal_loader):
+    return env_with_terminal_loader.get_template("pluglets/tile_grid/templates/j2-partials/tiles.html")
 
 
 class TestTileGridPluglet():
 
-    def test_no_input(self):
-        macro_output = tile_grid()
-        assert "USAGE" in macro_output
-
+    @patch('terminal.pluglets.tile_grid.MACRO')
     @pytest.mark.parametrize("bad_input", [
         pytest.param(
             None, id="null_input"
@@ -22,6 +26,25 @@ class TestTileGridPluglet():
             "tiles", id="string_input"
         )
     ])
-    def test_bad_input(self, bad_input):
-        macro_output = tile_grid()
-        assert "USAGE" in macro_output
+    # NOTE: the order of the inputs passed to the test is important here
+    # the patched mock needs to come before the pytest param
+    # see https://github.com/hackebrot/pytest-tricks/issues/32
+    def test_usage_message_when_bad_input(self, pluglet_macro_mock, bad_input):
+        pluglet_output = tile_grid(bad_input)
+        assert "USAGE" in pluglet_output
+        pluglet_macro_mock.assert_not_called()
+
+    @patch('terminal.pluglets.tile_grid.MACRO')
+    def test_pluglet_renders_grid_with_one_tile(self, pluglet_macro_mock, minimal_linked_image_tile, grid_partial):
+        mock_jinja2_env = MagicMock()
+        mock_jinja2_env_attr = {'get_template.return_value': grid_partial}
+        mock_jinja2_env.configure_mock(**mock_jinja2_env_attr)
+        pluglet_macro_mock_attr = {'jinja2_env': mock_jinja2_env}
+        pluglet_macro_mock.configure_mock(**pluglet_macro_mock_attr)
+
+        minimal_linked_image_tile.tile_id = "myTileId"
+        page_meta = {"tiles": [minimal_linked_image_tile]}
+        pluglet_output = tile_grid(page_meta)
+        assert "USAGE" not in pluglet_output
+        assert "id=\"myTileId\"" in pluglet_output
+        assert_valid_html(pluglet_output)
