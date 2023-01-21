709d95c24db571b74ba2ab30a20ef7103516ad3f
ntno <ntno@users.noreply.github.com>
2023-01-20 19:21:20 -0500

    test stubs

--- tests/pluglets/test_tile_grid_pluglet.py
+++ tests/pluglets/test_tile_grid_pluglet.py
@@ -1,4 +1,5 @@
 from terminal.pluglets.tile_grid.util import tile_grid
+import pytest
 
 
 class TestTileGridPluglet():
@@ -6,3 +7,11 @@ class TestTileGridPluglet():
     def test_null_input(self):
         macro_output = tile_grid(None)
         assert "USAGE" in macro_output
+
+    @pytest.mark.skip(reason="TODO")
+    def test_empty_input(self):
+        pass
+
+    @pytest.mark.skip(reason="TODO")
+    def test_missing_required_input(self):
+        pass
