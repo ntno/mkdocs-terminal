d67c561fc16bcdde60b20c5ca46525eef2ace76a
ntno <ntno@users.noreply.github.com>
2023-01-21 12:32:14 -0500

    wip

--- tests/pluglets/tile_grid/test_pluglet_setup.py
+++ tests/pluglets/tile_grid/test_pluglet_setup.py
@@ -1,11 +1,23 @@
+# from mkdocs.config.base import Config
+from mkdocs.config.defaults import MkDocsConfig
+from terminal.pluglets.tile_grid.macro import TileGridMacroEnvironment
 import pytest
 
 
-@pytest.mark.skip(reason="TODO")
 class TestTileGridPlugletSetup():
 
     def test_that_markup_filter_not_added_when_plugin_disabled(self):
-        pass
+        macro = TileGridMacroEnvironment()
+        assert macro.jinja2_env is None
+        assert macro.macro_config is None
+        assert macro.chatter is None
+
+        mkdocs_config = MkDocsConfig()
+        filter_list = macro.create_jinja2_filters(mkdocs_config)
+        assert len(filter_list) == 0
+
+        # cleanup singleton after test
+        del macro
 
     @pytest.mark.parametrize("plugin_name", [
         pytest.param(
