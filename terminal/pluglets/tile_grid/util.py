024d9b899d6e037cbb1d105b14cb3309e76de67b
ntno <ntno@users.noreply.github.com>
2023-01-20 19:01:29 -0500

    provide default value

--- terminal/pluglets/tile_grid/util.py
+++ terminal/pluglets/tile_grid/util.py
@@ -1,8 +1,8 @@
 from copy import copy
 DEFAULT_GRID_PARTIAL_PATH = "pluglets/tile_grid/templates/j2-partials/tiles.html"
 
-def tile_grid(page_metadata):
-    if page_metadata is not None:
+def tile_grid(page_metadata={}):
+    if page_metadata is not None and page_metadata is dict:
         context_data = {
             "page": {
                 "meta": copy(page_metadata)
