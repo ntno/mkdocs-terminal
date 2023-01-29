087575a847d9b42f517d2203cfb575d88acc5510
ntno <ntno@users.noreply.github.com>
2023-01-29 15:19:07 -0500

    refactor wip

--- tests/tile_grid/test_grid_placement.py
+++ tests/tile_grid/test_grid_placement.py
@@ -3,7 +3,7 @@ from tests.utils.html import assert_valid_html
 from tests.interface import page_features, theme_plugins, theme_features
 import pytest
 PAGE_BASE_PARTIAL_PATH = "partials/page-base.html"
-
+INLINE_MACRO_CALL = "{{ tile_grid(page.meta) }}"
 
 @pytest.fixture
 def page_base_partial(env_with_terminal_loader):
@@ -32,36 +32,40 @@ def fully_enabled_config():
 
 class TestGridPlacement():
 
-    def test_that_grid_is_after_markdown_content_by_default(self):
-        pass
-
-    @pytest.mark.parametrize("grid_configuration, expected_placement", [
+    @pytest.mark.parametrize("grid_configuration, with_macro_call, expected_placement", [
         pytest.param(
-            {}, "after", id="default_values"
+            {}, False, "after", id="default_values"
         ),
         pytest.param(
-            {page_features.SHOW_TILES_FIRST: False}, "after", id="show_last"
+            {page_features.SHOW_TILES_FIRST: False}, False, "after", id="show_last"
         ),
         pytest.param(
-            {page_features.SHOW_TILES_FIRST: True}, "before", id="show_first"
+            {page_features.SHOW_TILES_FIRST: True}, False, "before", id="show_first"
         ),
         pytest.param(
-            {page_features.SHOW_TILES_INLINE: True}, "inline", id="show_inline"
+            {page_features.SHOW_TILES_INLINE: True}, True, "inline", id="show_inline"
         ),
         pytest.param(
             {
                 page_features.SHOW_TILES_INLINE: True,
                 page_features.SHOW_TILES_FIRST: True
-            }, "inline", id="inline_overrides_first"
+            }, 
+            True,
+            "inline", 
+            id="inline_overrides_first"
         ),
     ])
-    def test_that_grid_is_in_expected_place(self, grid_configuration, expected_placement, page_base_partial, tiles, fully_enabled_config):
+    def test_that_grid_is_in_expected_place(self, grid_configuration, with_macro_call, expected_placement, page_base_partial, tiles, fully_enabled_config):
         tiles_meta = {"tiles": [tiles]}
         revision_meta = {"revision_date": "revision_date_placeholder"}
         page_meta = {**grid_configuration, **tiles_meta, **revision_meta}
+        if(with_macro_call):
+            page_content = "markdown_content_placeholder\n    " + INLINE_MACRO_CALL
+        else:
+            page_content = "markdown_content_placeholder" 
         context_data = {
             "page": {
-                "content": "markdown_content_placeholder",
+                "content": page_content,
                 "meta": page_meta
             },
             **fully_enabled_config
@@ -77,9 +81,11 @@ class TestGridPlacement():
         grid_indicator = "<div class=\"terminal-mkdocs-tile-grid \">"
         if (expected_placement == "before"):
             assert before_content_indicator in rendered_page
+            assert grid_indicator in rendered_page
         elif (expected_placement == "after"):
             assert after_content_indicator in rendered_page
+            assert grid_indicator in rendered_page
         elif (expected_placement == "inline"):
             assert before_content_indicator not in rendered_page
             assert after_content_indicator not in rendered_page
-        assert grid_indicator in rendered_page
+            assert grid_indicator not in rendered_page
