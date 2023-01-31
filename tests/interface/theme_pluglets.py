2cbf8b339010dae78e98c6abf3d6d4226a804cf3
ntno <ntno@users.noreply.github.com>
2023-01-30 23:30:08 -0500

    test fix

--- tests/interface/theme_pluglets.py
+++ tests/interface/theme_pluglets.py
@@ -1,3 +1,3 @@
 TILE_GRID_MACRO_USAGE_MESSAGE = "<p class=\"terminal-mkdocs-macro-error-banner\"><strong>USAGE:</strong>&nbsp;&nbsp;<code>{{ tile_grid(page.meta) }}</code></p>"
 TILE_GRID_MACRO_SECTION_START_TAG = "<section id=\"terminal-mkdocs-tile-grid-macro-output\">"
-TILE_LINK_START_TAG = "<div class=\"terminal-mkdocs-tile-link\"><a>"
+TILE_LINK_START_TAG = "<div class=\"terminal-mkdocs-tile-link\"><a "
