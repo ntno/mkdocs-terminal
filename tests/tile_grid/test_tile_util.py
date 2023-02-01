6a258132c34350c0ddd50f1eeefa64c3a4b28cfe
ntno <ntno@users.noreply.github.com>
2023-02-01 17:40:29 -0500

    wip

--- tests/tile_grid/test_tile_util.py
+++ tests/tile_grid/test_tile_util.py
@@ -1,7 +1,4 @@
 from tests.interface.tile import Tile
-from tests.utils.html import assert_valid_html, tile_has_anchor, tile_has_img
-from tests.utils.filters import mock_markup_filter
-from tests import defaults
 import pytest
 TILE_UTIL_MACRO_PATH = "pluglets/tile_grid/templates/j2-macros/tile-util.j2"
 
@@ -14,14 +11,44 @@ def tile_util_macro(env_with_terminal_loader):
 class TestTileUtil():
 
     def test_empty_tile_is_empty(self, tile_util_macro, empty_tile):
-        assert "false" == tile_util_macro.module.has_link(empty_tile)
-        assert "false" == tile_util_macro.module.has_image(empty_tile)
-        assert "false" == tile_util_macro.module.has_caption(empty_tile)
-        assert "false" == tile_util_macro.module.is_valid(empty_tile)
+        tile = empty_tile
+        assert "false" == tile_util_macro.module.has_link(tile)
+        assert "false" == tile_util_macro.module.has_image(tile)
+        assert "false" == tile_util_macro.module.has_caption(tile)
+        assert "false" == tile_util_macro.module.is_valid(tile)
+        assert "false" == tile_util_macro.module.is_link_only(tile)
+        assert "false" == tile_util_macro.module.is_image_only(tile)
+        assert "false" == tile_util_macro.module.is_linked_tile(tile)
 
     def test_minimal_link_tile_is_valid(self, tile_util_macro, minimal_link_tile):
-        assert "true" == tile_util_macro.module.has_link(minimal_link_tile)
-        assert "false" == tile_util_macro.module.has_image(minimal_link_tile)
-        assert "false" == tile_util_macro.module.has_caption(minimal_link_tile)
-        assert "true" == tile_util_macro.module.is_valid(minimal_link_tile)
-        assert "true" == tile_util_macro.module.is_link_only(minimal_link_tile)        
+        tile = minimal_link_tile
+        assert "true" == tile_util_macro.module.has_link(tile)
+        assert "true" == tile_util_macro.module.is_valid(tile)
+        assert "true" == tile_util_macro.module.is_link_only(tile)
+
+        assert "false" == tile_util_macro.module.has_image(tile)
+        assert "false" == tile_util_macro.module.has_caption(tile)
+        assert "false" == tile_util_macro.module.is_image_only(tile)
+        assert "false" == tile_util_macro.module.is_linked_tile(tile)
+
+    def test_minimal_image_tile_is_valid(self, tile_util_macro, minimal_image_tile):
+        tile = minimal_image_tile
+        assert "true" == tile_util_macro.module.has_image(tile)
+        assert "true" == tile_util_macro.module.is_valid(tile)
+        assert "true" == tile_util_macro.module.is_image_only(tile)
+
+        assert "false" == tile_util_macro.module.has_link(tile)        
+        assert "false" == tile_util_macro.module.has_caption(tile)
+        assert "false" == tile_util_macro.module.is_link_only(tile)
+        assert "false" == tile_util_macro.module.is_linked_tile(tile)
+
+    def test_minimal_linked_image_tile_is_valid(self, tile_util_macro, minimal_linked_image_tile):
+        tile = minimal_linked_image_tile
+        assert "true" == tile_util_macro.module.has_link(tile)
+        assert "true" == tile_util_macro.module.has_image(tile)
+        assert "true" == tile_util_macro.module.is_valid(tile)
+        assert "true" == tile_util_macro.module.is_linked_tile(tile)
+
+        assert "false" == tile_util_macro.module.has_caption(tile)
+        assert "false" == tile_util_macro.module.is_link_only(tile)
+        assert "false" == tile_util_macro.module.is_image_only(tile)
