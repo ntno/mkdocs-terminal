5276e510fb90dd587ecfb831aac31e143fae8171
ntno <ntno@users.noreply.github.com>
2025-08-17 14:21:43 -0400

    navigation building wip - based on mkdocs/mkdocs tests

---
+++ tests/integration_base.py
@@ -0,0 +1,29 @@
+# https://github.com/mkdocs/mkdocs/blob/master/mkdocs/tests/base.py
+# test helpers copied from mkdocs/mkdocs
+
+from __future__ import annotations
+
+import os
+import textwrap
+
+from mkdocs.config.defaults import MkDocsConfig
+
+def dedent(text):
+    return textwrap.dedent(text).strip()
+
+def load_config(config_file_path: str | None = None, **cfg) -> MkDocsConfig:
+    """Helper to build a simple config for testing."""
+    path_base = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'integration', 'base')
+    if 'site_name' not in cfg:
+        cfg['site_name'] = 'Example'
+    if 'docs_dir' not in cfg:
+        # Point to an actual dir to avoid a 'does not exist' error on validation.
+        cfg['docs_dir'] = os.path.join(path_base, 'docs')
+    if 'plugins' not in cfg:
+        cfg['plugins'] = []
+    conf = MkDocsConfig(config_file_path=config_file_path or os.path.join(path_base, 'mkdocs.yml'))
+    conf.load_dict(cfg)
+
+    errors_warnings = conf.validate()
+    assert errors_warnings == ([], []), errors_warnings
+    return conf
